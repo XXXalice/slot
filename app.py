dbfb289e931ca2d70737ea0fd4d3efa3e9e0fc87
梅宮 <euthanasia045@gmail.com>
2023-03-11 04:25:57 +0900

    ignore

---
+++ app.py
@@ -0,0 +1,19 @@
+from src.anaslo import Anaslo
+
+default_shop = "四谷タイガー"
+default_year = "2023"
+
+def main():
+    print("アナスロゲッター ver1.0")
+    print("現在のターゲット店舗：{}".format(default_shop))
+    print("現在のターゲット西暦：{}".format(default_year))
+    date = input("取得したい日付を入力してください（例 0311）：").strip()
+    get_data(date=date)
+
+def get_data(date):
+    anaslo = Anaslo(shop_name=default_shop, date=date)
+    rows = anaslo.fetch()
+    anaslo.to_csv(rows=rows)
+
+if __name__ == '__main__':
+    main()
